91b3972a753c415f90fba68aea65142b71c59c27
RogueMaster <RogueMaster@users.noreply.github.com>
2022-08-19 00:55:15 -0400

    Update ntagToBIN.sh

--- scripts/ntagToBIN.sh
+++ scripts/ntagToBIN.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+echo "Thanks ide for the converter... "
 set -euo pipefail
 echo -e -n "$(grep 'Page \d\+: .*' "$1" | cut -d: -f2 | tr -d '\n' | sed 's/ /\\x/g')" > "${1%.nfc}.bin"
